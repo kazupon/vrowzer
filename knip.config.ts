68fcd2b29b323202d45846272bc6dcf03d0b10dd
kazuya kawaguchi <kawakazu80@gmail.com>
2025-12-19 23:13:05 +0900

    add vite repo as sub module

--- knip.config.ts
+++ knip.config.ts
@@ -6,5 +6,6 @@ export default {
       entry: ['src/main.ts', 'src/preview/*.ts', 'src/worker/*.ts']
     }
   },
+  ignore: ['vite/**'],
   ignoreDependencies: ['lint-staged', '@kazupon/prettier-config']
 } satisfies KnipConfig
