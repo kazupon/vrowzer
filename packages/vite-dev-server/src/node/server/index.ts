a8924e6ba764dd7de8d215cfde39459a66573b09
kazuya kawaguchi <kawakazu80@gmail.com>
2026-01-20 14:33:23 +0900

    fix(vite-dev-server): minimal createServer logic

--- packages/vite-dev-server/src/node/server/index.ts
+++ packages/vite-dev-server/src/node/server/index.ts
@@ -10,7 +10,9 @@ import type { BindCLIShortcutsOptions, ShortcutsState } from '../shortcuts'
 
 // ...
 
-import type { Hono } from 'hono'
+import { Hono } from 'hono'
+import { handle } from 'hono/service-worker'
+import type { SvcWorker } from '@vrowser/service-worker/worker'
 import type { Env, BlankSchema } from 'hono/types'
 
 /**
@@ -52,25 +54,134 @@ export interface ViteDevServer {
    * ```
    */
   middlewares: Hono<ViteEnv, BlankSchema, '/'>
-
-  // TODO: fill in later
+  // TODO: fill in later ...
+  // ...
+  /**
+   * Stop the server.
+   */
+  close(): Promise<void>
+  /**
+   * Print server urls
+   */
+  // TODO: fill in later ...
+  // ...
 }
 
 
-export function createServer(
+export async function createServer(
+  serviceWorker: SvcWorker,
   inlineConfig: InlineConfig | ResolvedConfig = {},
-): Promise<ViteDevServer> {
-  return _createServer(inlineConfig, { listen: true })
-}
-
-export async function _createServer(
-  inlineConfig: ResolvedConfig | InlineConfig | undefined = {},
   options: {
     listen: boolean
     previousEnvironments?: Record<string, DevEnvironment>
     previousShortcutsState?: ShortcutsState<ViteDevServer>
   },
 ): Promise<ViteDevServer> {
-  // TODO: implement the server creation logic!
-  return {} as Promise<ViteDevServer>
+  // TOOD: implement for config resolving and etc ...
+  // ...
+  const config = inlineConfig as ResolvedConfig
+
+  // TODO: ...
+
+  const middlewares = new Hono<ViteEnv, BlankSchema, '/'>()
+
+  // TODO: ...
+
+  // Backward compatibility
+
+  // let moduleGraph = new ModuleGraph({
+  //   client: () => environments.client.moduleGraph,
+  //   ssr: () => environments.ssr.moduleGraph,
+  // })
+  // let pluginContainer = createPluginContainer(environments)
+
+  const closeHttpServer = createServerCloseFn(serviceWorker)
+
+  // const devHtmlTransformFn = createDevHtmlTransformFn(config)
+
+  // Promise used by `server.close()` to ensure `closeServer()` is only called once
+  let closeServerPromise: Promise<void> | undefined
+  const closeServer = async () => {
+    // if (!middlewareMode) {
+    //   teardownSIGTERMListener(closeServerAndExit)
+    // }
+
+    await Promise.allSettled([
+      // watcher.close(),
+      // ws.close(),
+      // Promise.allSettled(
+      //   Object.values(server.environments).map((environment) =>
+      //     environment.close(),
+      //   ),
+      // ),
+      closeHttpServer(),
+      // server._ssrCompatModuleRunner?.close(),
+    ])
+    // server.resolvedUrls = null
+    // server._ssrCompatModuleRunner = undefined
+  }
+
+  // let hot = ws
+  let server: ViteDevServer = {
+    config,
+    middlewares,
+    async close() {
+      if (!closeServerPromise) {
+        closeServerPromise = closeServer()
+      }
+      return closeServerPromise
+    },
+  }
+
+  // TODO: setup for HMR, watchers ...
+  // ...
+
+  // Pre applied internal middlewares ------------------------------------------
+
+  // TODO: setup internal middlewares
+
+  // apply configureServer post hooks ------------------------------------------
+
+  // TODO: setup for configureServer hooks
+
+  return server
+}
+
+// ...
+
+export function createServerCloseFn(
+  serviceWorker: SvcWorker,
+): () => Promise<void> {
+  // TODO: we need to manage hrm connections here too ?
+
+  // let hasListened = false
+  // const openSockets = new Set<net.Socket>()
+
+  // server.on('connection', (socket) => {
+  //   openSockets.add(socket)
+  //   socket.on('close', () => {
+  //     openSockets.delete(socket)
+  //   })
+  // })
+
+  // server.once('listening', () => {
+  //   hasListened = true
+  // })
+
+  return () =>
+    new Promise<void>((resolve, reject) => {
+      // TODO: destroy HMR connections too ?
+      // openSockets.forEach((s) => s.destroy())
+      // if (hasListened) {
+      //   server.close((err) => {
+      //     if (err) {
+      //       reject(err)
+      //     } else {
+      //       resolve()
+      //     }
+      //   })
+      // } else {
+      //   resolve()
+      // }
+    })
 }
