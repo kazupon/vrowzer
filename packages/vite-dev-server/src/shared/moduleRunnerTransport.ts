7cb2ecee4a4592c263b9bd2eed1605fd550e2cd8
kazuya kawaguchi <kawakazu80@gmail.com>
2026-01-27 18:10:46 +0900

    fix(vite-dev-server): remain shared/moduleRunnerTransport implementation

--- packages/vite-dev-server/src/shared/moduleRunnerTransport.ts
+++ packages/vite-dev-server/src/shared/moduleRunnerTransport.ts
@@ -1,12 +1,13 @@
 // TODO: fill in later ...
-// import { safeMessagePort } from '@kazupon/jts-utils/message/port'
-
-import type { HotPayload } from '#types/hmrPayload'
-import { safeMessagePort } from '@kazupon/jts-utils/message'
-
+import type { CustomPayload, HotPayload } from '#types/hmrPayload'
+import { safeMessagePort } from '@kazupon/jts-utils/message/port'
+import { nanoid } from 'nanoid/non-secure'
 import type {
-  InvokeMethods
+  InvokeMethods,
+  InvokeResponseData,
+  InvokeSendData,
 } from './invokeMethods'
+import { promiseWithResolvers } from './utils'
 
 export type ModuleRunnerTransportHandlers = {
   onMessage: (data: HotPayload) => void
@@ -41,7 +42,139 @@ function reviveInvokeError(e: any) {
   return error
 }
 
-// TODO: fill in later ...
+const createInvokeableTransport = (
+  transport: ModuleRunnerTransport,
+): InvokeableModuleRunnerTransport => {
+  if (transport.invoke) {
+    return {
+      ...transport,
+      async invoke(name, data) {
+        const result = await transport.invoke!({
+          type: 'custom',
+          event: 'vite:invoke',
+          data: {
+            id: 'send',
+            name,
+            data,
+          } satisfies InvokeSendData,
+        } satisfies CustomPayload)
+        if ('error' in result) {
+          throw reviveInvokeError(result.error)
+        }
+        return result.result
+      },
+    }
+  }
+
+  if (!transport.send || !transport.connect) {
+    throw new Error(
+      'transport must implement send and connect when invoke is not implemented',
+    )
+  }
+
+  const rpcPromises = new Map<
+    string,
+    {
+      resolve: (data: any) => void
+      reject: (data: any) => void
+      name: string
+      timeoutId?: ReturnType<typeof setTimeout>
+    }
+  >()
+
+  return {
+    ...transport,
+    connect({ onMessage, onDisconnection }) {
+      return transport.connect!({
+        onMessage(payload) {
+          if (payload.type === 'custom' && payload.event === 'vite:invoke') {
+            const data = payload.data as InvokeResponseData
+            if (data.id.startsWith('response:')) {
+              const invokeId = data.id.slice('response:'.length)
+              const promise = rpcPromises.get(invokeId)
+              if (!promise) return
+
+              if (promise.timeoutId) clearTimeout(promise.timeoutId)
+
+              rpcPromises.delete(invokeId)
+
+              const { error, result } = data.data
+              if (error) {
+                promise.reject(error)
+              } else {
+                promise.resolve(result)
+              }
+              return
+            }
+          }
+          onMessage(payload)
+        },
+        onDisconnection,
+      })
+    },
+    disconnect() {
+      rpcPromises.forEach((promise) => {
+        promise.reject(
+          new Error(
+            `transport was disconnected, cannot call ${JSON.stringify(promise.name)}`,
+          ),
+        )
+      })
+      rpcPromises.clear()
+      return transport.disconnect?.()
+    },
+    send(data) {
+      return transport.send!(data)
+    },
+    async invoke<T extends keyof InvokeMethods>(
+      name: T,
+      data: Parameters<InvokeMethods[T]>,
+    ) {
+      const promiseId = nanoid()
+      const wrappedData: CustomPayload = {
+        type: 'custom',
+        event: 'vite:invoke',
+        data: {
+          name,
+          id: `send:${promiseId}`,
+          data,
+        } satisfies InvokeSendData,
+      }
+      const sendPromise = transport.send!(wrappedData)
+
+      const { promise, resolve, reject } =
+        promiseWithResolvers<ReturnType<Awaited<InvokeMethods[T]>>>()
+      const timeout = transport.timeout ?? 60000
+      let timeoutId: ReturnType<typeof setTimeout> | undefined
+      if (timeout > 0) {
+        timeoutId = setTimeout(() => {
+          rpcPromises.delete(promiseId)
+          reject(
+            new Error(
+              `transport invoke timed out after ${timeout}ms (data: ${JSON.stringify(wrappedData)})`,
+            ),
+          )
+        }, timeout)
+        timeoutId?.unref?.()
+      }
+      rpcPromises.set(promiseId, { resolve, reject, name, timeoutId })
+
+      if (sendPromise) {
+        sendPromise.catch((err) => {
+          clearTimeout(timeoutId)
+          rpcPromises.delete(promiseId)
+          reject(err)
+        })
+      }
+
+      try {
+        return await promise
+      } catch (err) {
+        throw reviveInvokeError(err)
+      }
+    },
+  }
+}
 
 export interface NormalizedModuleRunnerTransport {
   connect?(onMessage?: (data: HotPayload) => void): Promise<void> | void
@@ -56,9 +189,72 @@ export interface NormalizedModuleRunnerTransport {
 export const normalizeModuleRunnerTransport = (
   transport: ModuleRunnerTransport,
 ): NormalizedModuleRunnerTransport => {
-  // TODO: implement later ...
+  const invokeableTransport = createInvokeableTransport(transport)
 
-  return {} as NormalizedModuleRunnerTransport
+  let isConnected = !invokeableTransport.connect
+  let connectingPromise: Promise<void> | undefined
+
+  return {
+    ...(transport as Omit<ModuleRunnerTransport, 'connect'>),
+    ...(invokeableTransport.connect
+      ? {
+        async connect(onMessage) {
+          if (isConnected) return
+          if (connectingPromise) {
+            await connectingPromise
+            return
+          }
+
+          const maybePromise = invokeableTransport.connect!({
+            onMessage: onMessage ?? (() => { }),
+            onDisconnection() {
+              isConnected = false
+            },
+          })
+          if (maybePromise) {
+            connectingPromise = maybePromise
+            await connectingPromise
+            connectingPromise = undefined
+          }
+          isConnected = true
+        },
+      }
+      : {}),
+    ...(invokeableTransport.disconnect
+      ? {
+        async disconnect() {
+          if (!isConnected) return
+          if (connectingPromise) {
+            await connectingPromise
+          }
+          isConnected = false
+          await invokeableTransport.disconnect!()
+        },
+      }
+      : {}),
+    async send(data) {
+      if (!invokeableTransport.send) return
+
+      if (!isConnected) {
+        if (connectingPromise) {
+          await connectingPromise
+        } else {
+          throw new Error('send was called before connect')
+        }
+      }
+      await invokeableTransport.send(data)
+    },
+    async invoke(name, data) {
+      if (!isConnected) {
+        if (connectingPromise) {
+          await connectingPromise
+        } else {
+          throw new Error('invoke was called before connect')
+        }
+      }
+      return invokeableTransport.invoke(name, data)
+    },
+  }
 }
 
 // MessageChannel connection events
