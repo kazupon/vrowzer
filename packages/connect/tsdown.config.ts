4482cfc210259dfa0cf5440d0ddefd5d0bf75ce8
kazuya kawaguchi <kawakazu80@gmail.com>
2026-01-29 13:20:39 +0900

    chore: remove connect related somethings

--- packages/connect/tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsdown'
-
-export default defineConfig({
-  entry: ['./src/index.ts', './src/incoming.ts', './src/outgoing.ts', './src/connect.ts'],
-  clean: true,
-  publint: true,
-  dts: true,
-  fixedExtension: false
-})
