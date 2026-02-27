952ca85ce186e449f2063fc9c6b9ada9d928adb6
kazuya kawaguchi <kawakazu80@gmail.com>
2026-02-27 19:53:13 +0900

    fix(vrowser): tweak vrowser structure

--- packages/vrowser/tsdown.config.ts
+++ packages/vrowser/tsdown.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'tsdown'
 
-const config: ReturnType<typeof defineConfig> = defineConfig([
-  {
-    platform: 'browser',
-    entry: ['./src/preview.ts'],
-    publint: true,
-    dts: true,
-    fixedExtension: false
-  },
-  {
-    platform: 'node',
-    entry: ['./src/vite.ts'],
-    publint: true,
-    dts: true
-  }
-])
+const config: ReturnType<typeof defineConfig> = defineConfig({
+  platform: 'browser',
+  entry: ['./src/index.ts'],
+  publint: true,
+  dts: true,
+  fixedExtension: false
+})
 
 export default config
