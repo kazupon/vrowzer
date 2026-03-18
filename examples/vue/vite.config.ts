ae48578cf22e299352dbb5b502f73f46144161d5
kazuya kawaguchi <kawakazu80@gmail.com>
2026-03-18 19:12:11 +0900

    fix(examples): updates

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -1,7 +1,12 @@
+import { DevTools } from '@vitejs/devtools'
 import vue from '@vitejs/plugin-vue'
 import * as compiler from 'vue/compiler-sfc'
 import { Vrowser } from '@vrowser/vite-plugin'
 import { defineConfig } from 'vite'
+
+process.env.VITE_DEVTOOLS_DISABLE_CLIENT_AUTH = 'true'
+
+const devtoolsPlugins = await DevTools()
 
 export default defineConfig({
   optimizeDeps: {
@@ -10,7 +15,8 @@ export default defineConfig({
   plugins: [
     vue({ compiler }),
     Vrowser({
-      experimental: { ide: true }
-    })
+      experimental: { ide: true, devtools: true }
+    }),
+    ...devtoolsPlugins.filter(p => p.name !== 'vite:devtools:injection')
   ]
 })
